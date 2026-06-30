73473a9e9c43959a805abcfbad8a387f213125ab
cseaditya-000 <aditya3546raj@gmail.com>
2026-06-30 15:12:55 +0530

    -

--- Beau_Tutorial/main.py
+++ Beau_Tutorial/main.py
@@ -11,32 +11,51 @@
 # range for ranges
 # dict for dictionaries
 # set for sets
+
 # ********Operator**********
 # = is assignment operator.
 # +(addition),-(subtraction),*(multiplication),/(division),%(modulus),**(square),//(floor division) are mathematical operators
 # age=83#This operator could be used with any mathematical operator. They means similar. This used to shorten the code.
 # age+=9#this means age=age +9
 # print(age)
+
 # ==,!=,>,<,<=,>= are comparison operators.
 # == check whether equal to
 # !- checks whether not equal to. Others are obvious.
+
 # Boolean operator True or False
+
 ## OR Operator ## Checks first if first is true then returns the first and if it is fslse then it returns the second as output
+
 # print(0 or 1) #1 //0--fakse and 1--False
 # print(False or 'hey') #hey
 # print('hi' or 'hey') #hi
 # print([] or False) #False as [] is considerded a false value
 # print(False or []) # [] as Flase is also a false value
 # # AND Operator ## Evaluates whether first is true or false. if first is true then it moves on to second and if first is false then it gives the first value as output wihthout checking the second 
+
 # print(0 and 1) #0
 # print(1 and 0) #0
 # print(False and 'hey') #False
 # print('hi' and 'hey') #hey
 # print([] and False) #[]
 # print(False and []) #False
+
 ## BITWISE OPERATOR ##
+# & perfroms binary and
+# | performs binary or
+# ^ performs a binary xor(exclusive or) operation
+# ~ performs a binary nor operation
+# << shift left operation 
+# >> shift right operation
+
 # is--Identity Operator
+# a=1
+# print(int(a) == 1)
 # in--Memebership Operator
+# a=[1,2,3]
+# print(1 in a)
+
 #       Ternary Operator        # 
 # def is_adult(age):
 #     if age > 18:
@@ -44,7 +63,7 @@
 #     else:
 #         return False
     
-# def is_adult2(age):           #with teernary operator
+# def is_adult2(age):           #with ternary operator
 #     return True if age>18 else False #Everything on a single line
 
 #Strings in python 
@@ -52,18 +71,19 @@
 # 'Adi'
 # name='Adi'
 # phrase=name+' is my name'
-# name+=' is my name'
+# name+=' is my name'#append
 # print(name)
 # age=str(38)
+# type ctrl+/
 # print("""Adi is
-
-# 19
-      
+# 19     
 #       years old
 # """)#multi line string
+
 # print("adi".upper())#Changes to upper case all
 # print("ADi".lower())#changes to lower case all
 # print("adi doIng things".title())#Changes the first letter of each word to capital and remaing to small
 # print("adea".islower())#used to check whether it is all lower letters
-a=str(123)
-print(a+"j")
+
+# a=str(123)
+# print(a+"j")
